230152d8316f5ec00ca3801756aa54d44bc9545b
BYJRK <11771667+BYJRK@users.noreply.github.com>
2022-11-07 13:56:48 +0800

    Move styles into separate resource dictionaries
    Move styles into Style.xaml;
    Move brushes into Color.xaml;
    Move side panel into UserControl SidePanel.xaml

---
+++ src/Views/SidePanel.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace HonkaiCharacterBrowser.Views;
+
+public partial class SidePanel : UserControl
+{
+    public SidePanel()
+    {
+        InitializeComponent();
+    }
+}
