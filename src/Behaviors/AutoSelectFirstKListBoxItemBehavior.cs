b2ccb4f5bcedc88425237d2b7f424cad4a25a420
BYJRK <11771667+BYJRK@users.noreply.github.com>
2022-11-07 17:55:36 +0800

    Auto select first skill using custom behavior

---
+++ src/Behaviors/AutoSelectFirstKListBoxItemBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace HonkaiCharacterBrowser.Behaviors;
+
+public class AutoSelectFirstKListBoxItemBehavior : Behavior<Selector>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        AssociatedObject.TargetUpdated += AutoSelectFirst;
+    }
+
+    void AutoSelectFirst(object sender, DataTransferEventArgs e)
+    {
+        if (AssociatedObject.HasItems)
+            AssociatedObject.SelectedIndex = 0;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+
+        AssociatedObject.TargetUpdated -= AutoSelectFirst;
+    }
+}
